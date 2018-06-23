202fe92e1add92b9158a86bf954fbd58f677e2d1
kzhangkzhang <kezhangkezhang@gmail.com>
2018-06-22 21:05:09 -0500

    add function to hover over cells

--- connect4.js
+++ connect4.js
@@ -5,6 +5,7 @@ class Connect4 {
         this.selector   = selector;
 
         this.createGrid();
+        this.setupEventListeners();
     }
 
     createGrid() {
@@ -16,12 +17,43 @@ class Connect4 {
             const $row = $('<div>').addClass('row');
 
             for (let col = 0; col < this.COLS; col++) {
-                 const $col = $('<div>').addClass('col empty');
+                 const $col = $('<div>').addClass('col empty').attr('data-col', col).attr('data-row', row);
                  $row.append($col);
             }
 
             $board.append($row);
         }
         // console.log($board.html());
+    }
+
+    setupEventListeners() {
+
+        const $board = $(this.selector);
+
+        function findlastEmptyCell(col) {
+            const cells = $(`.col[data-col='${col}']`);
+
+            for (let i = cells.length - 1; i>= 0; i-- ) {
+                const $cell = $(cells[i]);
+                if ($cell.hasClass('empty')) {
+                    return $cell;
+                }
+            }
+            return null;
+        }
+
+        $board.on('mouseenter', '.col.empty', function() {
+            console.log('mouse enter ==>  ', this);
+
+            const col = $(this).data('col');
+
+            const $lastEmptyCell = findlastEmptyCell(col);
+            $lastEmptyCell.addClass(`next-red`);
+
+        });
+
+        $board.on('mouseleave', '.col', function() {
+            $('.col').removeClass('next-red');
+        })
     }
 }
